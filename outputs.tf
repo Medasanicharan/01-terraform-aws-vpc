ea50dc81987ab183a25bd766bac56c15ea8860ee
charan <medasanicharan66@gmail.com>
2025-02-11 17:18:43 +0530

    expense-terraform-dev

--- outputs.tf
+++ outputs.tf
@@ -23,6 +23,11 @@ output "database_subnet_group_ids" {
     value = aws_db_subnet_group.default.id 
 }
 
+output "database_subnet_group_name" {
+    value = aws_db_subnet_group.default.name
+}
+
+
 output "igw_id" {
     value = aws_internet_gateway.gw.id
 }
